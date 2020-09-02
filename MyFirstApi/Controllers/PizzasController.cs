1ce583e3df00d364832497fb1df90fed6229a684
Nathan Gonzalez <ncgonz@gmail.com>
2020-09-01 19:21:17 -0500

    Add project files.

---
+++ MyFirstApi/Controllers/PizzasController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstApi.Models;
+
+namespace MyFirstApi.Controllers
+{
+    [Route("api/pizzas")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+        [HttpGet]
+        public List<Pizza> GetAllPizzas()
+        {
+            var pizza = new Pizza {Id = 1,Size= "Medium",Toppings = new List<string>{"Pepperoni"}};
+            var pizza2 = new Pizza {Id = 2,Size= "Medium",Toppings = new List<string>{"Sausage"}};
+            var pizza3 = new Pizza {Id = 3,Size= "Medium",Toppings = new List<string>{"Bacon"}};
+
+            return new List<Pizza> {pizza,pizza2,pizza3};
+        }
+    }
+}
---
+++ MyFirstApi/Models/Pizza.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstApi.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Size { get; set; }
+        public List<string> Toppings { get; set; }
+    }
+}
